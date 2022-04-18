2f4e712cacffe3f1e53e8eee44fbcecbd68b9fd1
Fatema-Nawrin <nawrin4000@gmail.com>
2022-04-18 23:20:22 +0600

    last update

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div style={{ color: 'rgb(25, 100, 130)' }}>
+        <div style={{ color: 'rgb(25, 100, 130)', height: '100vh' }}>
             <h1 className='text-center mt-5'>Oops!!</h1>
             <h4 className='text-center'>404 - PAGE NOT FOUND</h4>
         </div>
