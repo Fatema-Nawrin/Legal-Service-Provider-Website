f364909d97582029cb357fb2f95a9761a025b9fc
Fatema-Nawrin <nawrin4000@gmail.com>
2022-04-18 21:33:00 +0600

    blogs and 404 page added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 import RequireAuth from './Pages/Shared/RequireAuth/RequireAuth';
 
 function App() {
@@ -25,7 +26,7 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
-        <Route path='*'></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div >
---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div style={{ color: 'rgb(25, 100, 130)' }}>
+            <h1 className='text-center mt-5'>Oops!!</h1>
+            <h4 className='text-center'>404 - PAGE NOT FOUND</h4>
+        </div>
+    );
+};
+
+export default NotFound;
